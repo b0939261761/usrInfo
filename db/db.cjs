b82bcaa58c11483c915ce5de26f87ca563ef945b
b0939261761 <b0939261761@gmail.com>
2021-05-05 20:58:19 +0300

    clear

--- db/db.cjs
+++ db/db.cjs
@@ -1,9 +1,6 @@
 const { createPool } = require('slonik');
-const { createQueryLoggingInterceptor } = require('slonik-interceptor-query-logging');
-
-const interceptors = process.env.NODE_ENV !== 'production' ? [createQueryLoggingInterceptor()] : [];
 
 const connect = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}`
   + `@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-module.exports = createPool(connect, { interceptors });
+module.exports = createPool(connect);
